527c154852b7da209ffe4c4618039c4ae1856317
Łukasz Blachnicki <lblachnicki@gmail.com>
2021-01-19 13:46:26 +0100

    Add draft of solution for class 13

---
+++ src/Laboratories/Class_13/MergeSortMultithreaded.java
@@ -0,0 +1,142 @@
+package Laboratories.Class_13;
+
+//MERGE-SORT (T, p, r):
+//     IF p <r:
+//         q → (p + r) / 2
+//         MERGE-SORT  (T, p, q)
+//         MERGE-SORT  (T, q + 1, r)
+//         MERGE (T, p, q, r)
+
+import java.util.Arrays;
+
+class Sorter extends Thread {
+    int[] internal;
+
+    public Sorter(int[] array) {
+        this.internal = array;
+    }
+
+    public void run() {
+        mergeSort(internal);
+    }
+
+    public int[] getResult() {
+        return internal;
+    }
+
+    public void mergeSort(int[] array) {
+        if (array.length > 1) {
+            int[] left = leftHalf(array);
+            int[] right = rightHalf(array);
+
+            mergeSort(left);
+            mergeSort(right);
+
+            merge(array, left, right);
+        }
+    }
+
+    public int[] leftHalf(int[] array) {
+        int size1 = array.length / 2;
+        int[] left = new int[size1];
+        for (int i = 0; i < size1; i++) {
+            left[i] = array[i];
+        }
+        return left;
+    }
+
+    public int[] rightHalf(int[] array) {
+        int size1 = array.length / 2;
+        int size2 = array.length - size1;
+        int[] right = new int[size2];
+        for (int i = 0; i < size2; i++) {
+            right[i] = array[i + size1];
+        }
+        return right;
+    }
+
+    public void merge(int[] result, int[] left, int[] right) {
+        int i1 = 0;
+        int i2 = 0;
+
+        for (int i = 0; i < result.length; i++) {
+            if (i2 >= right.length || (i1 < left.length && left[i1] <= right[i2])) {
+                result[i] = left[i1];
+                i1++;
+            } else {
+                result[i] = right[i2];
+                i2++;
+            }
+        }
+
+        this.internal = result;
+    }
+}
+
+public class MergeSortMultithreaded {
+    int threads = Runtime.getRuntime().availableProcessors();
+
+    public static int[] finalMerge(int[] a, int[] b) {
+        int[] result = new int[a.length + b.length];
+        int i = 0;
+        int j = 0;
+        int r = 0;
+        while (i < a.length && j < b.length) {
+            if (a[i] <= b[j]) {
+                result[r] = a[i];
+                i++;
+                r++;
+            } else {
+                result[r] = b[j];
+                j++;
+                r++;
+            }
+            if (i == a.length) {
+                while (j < b.length) {
+                    result[r] = b[j];
+                    r++;
+                    j++;
+                }
+            }
+            if (j == b.length) {
+                while (i < a.length) {
+                    result[r] = a[i];
+                    r++;
+                    i++;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private static int[] mergeSort(int[] originalArray) throws InterruptedException {
+        int[] subArray1 = new int[originalArray.length / 2];
+        int[] subArray2 = new int[originalArray.length - originalArray.length / 2];
+        System.arraycopy(originalArray, 0, subArray1, 0, originalArray.length / 2);
+        System.arraycopy(originalArray, originalArray.length / 2, subArray2, 0, originalArray.length - originalArray.length / 2);
+
+        Sorter sorter1 = new Sorter(subArray1);
+        Sorter sorter2 = new Sorter(subArray2);
+
+        sorter1.start();
+        sorter2.start();
+
+        // wait until finish
+        sorter1.join();
+        sorter2.join();
+
+        return finalMerge(sorter1.getResult(), sorter2.getResult());
+    }
+
+    // pass threads as parameter
+    public static void main(String[] args) throws InterruptedException {
+        int[] unsortedArray = new int[]{10, -9, 20, 100, 50};
+
+        System.out.println(Arrays.toString(mergeSort(unsortedArray)));
+
+        assert (mergeSort(new int[]{10, -9, 20, 100, 50}) == new int[]{-9, 1, 20, 50, 100});
+        assert (mergeSort(new int[]{}) == new int[]{});
+        assert (mergeSort(new int[]{1, 2, 3, 4, 5, 6}) == new int[]{1, 2, 3, 4, 5, 6});
+    }
+}
